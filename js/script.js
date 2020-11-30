da7ef39b2deb448fc58d390975feaf6191386087
Davide <ciampad06@gmail.com>
2020-11-30 16:44:58 +0100

    fase aggiunta oggetto

--- js/script.js
+++ js/script.js
@@ -8,17 +8,39 @@ $(document).ready(
       "comprare le uova",
       "comprare i pomodori",
       "comprare le patate"
-    ]
+    ];
 
     var source = $("#entry-template").html();
     var template = Handlebars.compile(source);
 
     for (var i = 0; i < list.length; i++){
+
       var context = {
-      text: list[i]
-    };
+        text: list[i]
+      };
+      var html = template(context);
+      $("#list").append(html);
     }
-    var html = template(context);
-    $("#list").append(html);
+
+
+    $(".delete").click(
+      function() {
+        $(this).parent().remove();
+      }
+    )
+
+    $("input").keypress(function(event){
+      if (event.which == 13 || event.keyCode == 13){
+        var newItem = $("[name='add']").val();
+
+        var context = {
+          text: newItem
+        };
+        var html = template(context);
+        $("#list").append(html);
+
+
+      }
+    });
   }
 );
